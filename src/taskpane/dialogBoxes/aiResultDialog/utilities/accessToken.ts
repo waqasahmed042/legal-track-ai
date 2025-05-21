18509e4186e64309017c4e55aee50280fbbe97a4
waqasahmed042 <waqasahmed.it042@gmail.com>
2025-05-21 09:36:43 +0500

    update pord setup

--- src/taskpane/dialogBoxes/aiResultDialog/utilities/accessToken.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-undef */
-/* eslint-disable prettier/prettier */
-export const Token = () => {
-    const accessToken = localStorage.getItem("access_token");
-
-    return accessToken;
-};
--- src/taskpane/utilities/trackChanges.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-/* eslint-disable prettier/prettier */
-export const addTrackingModeForEveryOne = async () => {
-    await Word.run(async (context) => {
-        const document: Word.Document = context.document;
-        document.changeTrackingMode = Word.ChangeTrackingMode.trackAll;
-        await context.sync();
-    });
-}
